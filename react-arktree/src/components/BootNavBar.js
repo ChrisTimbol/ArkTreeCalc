03c6d56a0817954995a122fef6385a9b99fd4697
Chris Timbol <christimbol707@gmail.com>
2022-03-28 12:30:05 -0700

    move hashrouter to render so navbars work

--- react-arktree/src/components/BootNavBar.js
+++ react-arktree/src/components/BootNavBar.js
@@ -4,8 +4,9 @@ import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Image from 'react-bootstrap/Image'
+import {Link} from 'react-router-dom';
 export default function BootNavBar() {
-
+ 
     return (
         <>
             <Navbar className="sticky-top" bg="dark" variant="dark" expand="lg">
@@ -17,10 +18,10 @@ export default function BootNavBar() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav ">
                         <Nav className="me-auto">
-                            <Nav.Link className="text-center text-light" href="/news"><h3>News</h3></Nav.Link>
-                            <Nav.Link className="text-center text-light" href="/calculator"><h3>Skill Calculator</h3></Nav.Link>
-                            <Nav.Link className="text-center text-light" href="/tierlist"><h3>Tier List</h3></Nav.Link>
-                            <Nav.Link className="text-center text-light" href="/patchnotes"><h3>Patch Notes</h3></Nav.Link>
+                            <Nav.Link as={Link} className="text-center text-light" to="/news"><h3>News</h3></Nav.Link>
+                            <Nav.Link as={Link} className="text-center text-light" to="/calculator"><h3>Skill Calculator</h3></Nav.Link>
+                            <Nav.Link as={Link} className="text-center text-light" to="/tierlist"><h3>Tier List</h3></Nav.Link>
+                            <Nav.Link as={Link} className="text-center text-light" to="/patchnotes"><h3>Patch Notes</h3></Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
